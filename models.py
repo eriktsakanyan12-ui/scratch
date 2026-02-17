63ed6689fb4dd50dd06e54710804b0a35c76d908
eriktsakanyan12-ui <eriktsakanyan12@gmail.com>
2026-02-17 13:17:57 +0400

    models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Perceptron:
   def __init__(self, num_inputs, learning_rate=0.01):
@@ -14,16 +15,51 @@ class Perceptron:
     return z
   
   def loss(self, prediction, target):
-    return target - prediction
+    return prediction - target 
   
-  def fit(self, X, y, tol=10e-5, epoch=100):
+  def fit(self, X, y, tol=1e-5, epoch=1000):
     self.history = []
+    n = len(X)
+
     for _ in range(epoch):
         y_pred = self.predict(X)
-        mse = self.loss(y_pred, y)**2
-        if mse<tol:
-            pass 
-        else:
-            self.weights[1:] = self.weights[1:]-2*X*mse**0.5
-            self.weights[0] = self.weights[0]-2*mse**0.5 
-            self.history.append(mse.mean())
+        error = y_pred - y
+        
+        mse = np.mean(error ** 2)
+        self.history.append(mse)
+
+        if mse < tol:
+            break
+
+        dw = (2/n) * np.dot(error, X)
+        db = (2/n) * np.sum(error)
+
+        self.weights[1:] -= self.learning_rate * dw
+        self.weights[0] -= self.learning_rate * db
+
+
+if __name__ == "__main__":
+   k, b = 1, 2
+
+   x = np.linspace(-10, 11, 1000)
+   x = x.reshape(-1, 1)
+
+   y = k*x + b
+   y = y.flatten()
+
+   random_int = (np.random.rand(1000) - 0.5) * 10
+   y_synt = y + random_int
+
+   nn = Perceptron(1, learning_rate=0.001)
+   nn.fit(x, y_synt)
+
+
+   plt.plot(x, y_synt, 'o', c = 'r')
+   plt.plot(x, y)
+   plt.show()
+   print(y)
+
+   nn = Perceptron(1)
+   nn.fit(x, y)
+   print(nn.predict(x))
+   print("Weights:", nn.weights)
